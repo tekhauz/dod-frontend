70aa9d08eafb105ded224a140511845cce40b19f
tekhauz <162763290+tekhauz@users.noreply.github.com>
2024-03-09 18:46:23 -0600

    Add files via upload

---
+++ utils/mongi.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+
+class MongoHandler:
+    def __init__(self, mongodb_connection_string):
+        self.client = MongoClient(mongodb_connection_string)
+        self.db = self.client["fpds"]
+        self.collection = self.db["contracts"]
+    
+    def insert_document(self, document):
+        return self.collection.insert_one(document)
+    
+    def find_document(self, query):
+        return self.collection.find_one(query)
+    
+    def update_document(self, query, update):
+        return self.collection.update_one(query, {"$set": update})
+    
+    def delete_document(self, query):
+        return self.collection.delete_one(query)
+    
+    def count_documents(self, query=None):
+        if query:
+            return self.collection.count_documents(query)
+        else:
+            return self.collection.estimated_document_count()
